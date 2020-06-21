3c5fd80563280fbd3cb56742887cb2a0e9e2301e
MaximeCroft <maximecroft@hotmail.com>
2020-06-21 17:46:32 +0200

    update fanflow.m

--- fcn/fanFlow.m
+++ fcn/fanFlow.m
@@ -7,12 +7,4 @@ function [mdotfan,Pin] = fanFlow(omega, par)
     %Calculate input power
     Pin = par.fan.p1*((omega/par.fan.omega1)^3);        % Power input fan [W]
     
-<<<<<<< HEAD
 end
-
-=======
-    % Air flow and mass flow
-    u = (4*Pin/(Cp*0.5*par.air.rho*D*D*pi))^(1/3);
-    mdot = 0.25*D^2*pi*u*par.air.rho;
-end
->>>>>>> 5d9adfe8b8cb59feb34eec6f957b6111d30c3385
