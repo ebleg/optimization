7192b741f7f96b909ce22f18931cae5e5a22eac3
Emiel Legrand <emiel.legrand@gmail.com>
2020-06-24 11:35:47 +0200

    beun bfgs

--- fcn/bfgs.m
+++ fcn/bfgs.m
@@ -6,32 +6,36 @@ function [xopt] = bfgs(fcn, x0, H0)
         x0 = x0';
     end
     
-    g1 = findif(fcn, x0);
+    n = size(x0, 2);
+    
+    g1 = jacobianest(fcn, x0)';
     
     % Convergence criterion
-    eps = 1e-3;
+    eps = 1e-5;
     i = 0; converged = false;
     
     % Obtain search direction
-    while ~converged && (i <= 1e4)
-        p = B*g1;
-        
-        a = lineSearch(@(a) fcn(x0 + a*p), [0 10]);
+    while ~converged && (i <= 200)
+        p = -B*g1;
+
+        a = fminbnd(@(a) fcn(x0 + a*p), 0, 3);
         s = a*p;
         x1 = x0 + s;
-        
-        g2 = findif(fcn, x1);
+
+        g2 = jacobianest(fcn, x1)';
         y = g2 - g1;
         
-        B = B + (s'*y + y'*B*y)/(s'*y)^2 - (B*y*s' + s*y'*B)/(s'*y);
+        rho = 1/(y'*s);
+        B = (eye(n) - rho*(s*y'))*B*(eye(n) - rho*(y*s')) + rho*(s*s');
         
         if norm(x1 - x0) < eps
             converged = true;
         end
         
-        x0 = x1; i = i + 1;
+        scatter(x0(1), x0(2), 'filled', 'blue');
+        x0 = x1; i = i + 1; g1 = g2;
     end
-       
+    disp(i)
     xopt = x1;
 end
 
--- scripts/testbfgs.m
+++ scripts/testbfgs.m
@@ -1,17 +1,24 @@
+clear; close all;
+figure; hold on;
+axis equal;
+
 f = @(x) (1 - x(1)).^2 + 100*(x(2) - x(1).^2).^2 ;
 
-[X, Y] = meshgrid(-10:0.1:10, -10:0.1:10);
+[X, Y] = meshgrid(-3:0.1:3, -3:0.1:3);
 Z = nan(size(X));
 
 for i = 1:numel(X)
     Z(i) = f([X(i), Y(i)]);
 end
 
-contour(X, Y, Z);
+contour(X, Y, Z, linspace(0, 1000, 30));
+grid; grid minor;
 
-x0 = [3, 4];
+x0 = [-2.1 -2.7];
 H0 = hessian(f, x0);
 xopt = bfgs(f, x0, H0);
 
+scatter(xopt(1), xopt(2), 'filled');
+
+
 hold on;
-% contour(xopt(1), xopt(2), f(xopt));
