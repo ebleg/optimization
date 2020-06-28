8088fc6d61f71c372dcd91cd1eb5857dd0c7ba54
MaximeCroft <maximecroft@hotmail.com>
2020-06-28 18:20:28 +0200

    update combined model and simpleproblemplot
    Boundary conditions getest, ergens zit een fout nog in deze versie ook

--- fcn/combinedModel.m
+++ fcn/combinedModel.m
@@ -1,18 +1,17 @@
-function [total, cost, meta] = combinedModel(omega, t, nlayers, par)
-    t = max(0, t);
-    omega = max(0, omega);
-    
-    % Fan flow
-    [mdot_fan, Pin] = fanFlow(omega, par);
-    
+function [cost] = combinedModel(omega, t, nlayers, par)
+% [total, cost, meta] = combinedModel(omega, t, nlayers, par)
+
+%     t = max(0, t);
+%     omega = max(0, omega);
+
     % Battery configuration
     [nch, stack, dim] = batteryLayout(t, nlayers, par);
+   
+    % Fan flow
+    [qfan,Pin] = fanFlow(omega, par);
     
-    % Channel area
-    [~, Ac, ~] = voidGeometry(t, par);
-    
-    % Mass flow distribution
-    u = mdot_fan/nch/Ac;  % mass conservation + equal distribution over channels
+    % Mass flow
+    [u] = massFlow(qfan, stack, t, par);
     
     % Thermal model
     Tcells = ones(stack.x, stack.y, stack.z)*par.cell.T0;
@@ -32,25 +31,59 @@ function [total, cost, meta] = combinedModel(omega, t, nlayers, par)
         end
 
         Tcells = TcellsNew;
+        Tmax = max(Tcells(:));
+        Tavg = mean(Tcells(:));
 
         i = i + 1;
     end
     
-    Tmax = smoothmax(Tcells(:), 1);
-    Tavg = mean(Tcells(:));
+%   Tmax = smoothmax(Tcells(:), 1);
+%     Tmax = max(Tcells(:));
+%     Tavg = mean(Tcells(:));
     
+    % Cost function
+    option = 8;
+    
+    if option == 1
     cost  = [boundaryFcn(dim.x, 0, par.cost.xwidth, par.cost.r_boundary), ...
              boundaryFcn(dim.y, 0, par.cost.ywidth, par.cost.r_boundary), ...
              boundaryFcn(dim.z, 0, par.cost.z, par.cost.r_boundary), ...
              boundaryFcn(Tmax, 0, par.cell.Tmax, par.cost.r_boundary), ...
              Tavg/300, ...
-             20*Pin/par.cost.P_nominal];
-       
-%     cost2 = 100*(Tavg - par.air.T)/(par.cell.Tmax - par.air.T) ...
-%              + 0*Pin/2/par.cost.P_max;
+             -20*Pin/par.cost.P_nominal];
+    elseif option == 2
+    cost  = [boundaryFcn(dim.x, 0, par.cost.xwidth, par.cost.r_boundary), ...
+             boundaryFcn(dim.y, 0, par.cost.ywidth, par.cost.r_boundary), ...
+             boundaryFcn(dim.z, 0, par.cost.z, par.cost.r_boundary), ...
+             boundaryFcn(Tmax, 0, par.cell.Tmax, par.cost.r_boundary), ...
+             Tavg/300, ...
+             10*Pin/par.cost.P_nominal];
+    elseif option == 3
+    cost1 = boundaryFcn(dim.x, 0, par.cost.xwidth, par.cost.r_boundary) + boundaryFcn(dim.y, 0, par.cost.ywidth, par.cost.r_boundary) + boundaryFcn(dim.z, 0, par.cost.z, par.cost.r_boundary) + boundaryFcn(Tmax, 0, par.cell.Tmax, par.cost.r_boundary);
+    cost2 = 100*(Tavg - par.air.T)/(par.cell.Tmax - par.air.T) + 0*Pin/2/par.cost.P_max;
+    cost = cost1 + cost2;
+    elseif option == 4
+    cost1 = boundaryFcn(dim.x, 0, par.cost.xwidth, par.cost.r_boundary) + boundaryFcn(dim.y, 0, par.cost.ywidth, par.cost.r_boundary) + boundaryFcn(dim.z, 0, par.cost.z, par.cost.r_boundary) + boundaryFcn(Tmax, 0, par.cell.Tmax, par.cost.r_boundary);
+    cost2 = Tavg/300 -20*Pin/par.cost.P_nominal;
+    cost = cost1 + cost2; 
+    elseif option == 5
+    cost1 = boundaryFcn(dim.x, 0, par.cost.xwidth, par.cost.r_boundary) + boundaryFcn(dim.y, 0, par.cost.ywidth, par.cost.r_boundary) + boundaryFcn(dim.z, 0, par.cost.z, par.cost.r_boundary) + boundaryFcn(Tmax, 0, par.cell.Tmax, par.cost.r_boundary);
+    cost2 = (Tavg-par.air.T)/(par.cell.Tmax-par.air.T) + (Pin/par.cost.P_max);
+    cost = cost1 + cost2; 
+    elseif option == 6
+    cost1 = boundaryFcn(dim.x, 0, par.cost.xwidth, par.cost.r_boundary) + boundaryFcn(dim.y, 0, par.cost.ywidth, par.cost.r_boundary) + boundaryFcn(dim.z, 0, par.cost.z, par.cost.r_boundary);
+    cost2 = (Tavg-par.air.T)/(par.cell.Tmax-par.air.T) + (Pin/par.cost.P_max);
+    cost = cost1 + cost2; 
+    elseif option == 7
+    cost = (Tavg-par.air.T)/(par.cell.Tmax-par.air.T) + (Pin/par.cost.P_max);
+    elseif option == 8
+    cost = (Tavg-par.air.T)/(par.cell.Tmax-par.air.T);
+    else 
+    cost = Pin/par.cost.P_max;
     
-    total = sum(cost);
-    meta.Tcells = Tcells;
+%     total = sum(cost);
+%     meta.Tcells = Tcells;
+
 
 end
 
--- scripts/simpleProblemPlot.m
+++ scripts/simpleProblemPlot.m
@@ -1,8 +1,11 @@
+clear all; close all; clc;
+
 t = linspace(0, 10e-3, 10);
-omega = linspace(0, 150, 10);
+omega = linspace(0, 300, 300);
 nlayers = 4;
 
 run params
+run defaultPlotSettings
 
 par.accu.ncells = 60;
 
@@ -11,6 +14,12 @@ cost = nan(size(trng));
 
 for i = 1:numel(trng)
     cost(i) = combinedModel(omegarng(i), trng(i), nlayers, par);
+%     plotSensitivityAnalysis(fun,xopt);
 end
 
 mesh(trng, omegarng, cost)
+xlabel('t [m]')
+ylabel('omega [rad/s]')
+
+
+% [cost] = combinedModel(omega, t, nlayers, par)
