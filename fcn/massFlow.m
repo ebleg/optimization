19406cfbee863f9850ec162ce202928a7f701fa4
MaximeCroft <maximecroft@hotmail.com>
2020-06-28 18:16:56 +0200

    update

--- fcn/massFlow.m
+++ fcn/massFlow.m
@@ -3,7 +3,7 @@ function [u] = massFlow(qfan, stack, t, par)
     %% Calculate the velocity of air through the channels 
     mdotfan = qfan*par.air.rho;                         % Mass flow fan
     mdot = mdotfan/((stack.x-1)*(stack.y-1));     % Mass flow through one channel [kg/s]
-    [Dh, Ac, P] = voidGeometry(t, par)
+    [Dh, Ac, P] = voidGeometry(t, par);
     u = mdot/(par.air.rho*Ac);                    % Velocity of air through channel [m/s]                       
     
 end
